f2d881ddfc5e32a5fff8151da65702835ece7ccf
jamesloui18 <45702624+jamesloui18@users.noreply.github.com>
2018-12-10 20:12:01 -0500

    Updating...

--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -20,15 +20,15 @@ public class KeyScript : MonoBehaviour {
         {// transform.position = new Vector3(Random.value, Random.value, 0)
             if (currentScene == 2)
             {
-                OpenedDoor.transform.position = new Vector3(transform.position.x - 5f, transform.position.y, transform.position.z);
+                OpenedDoor.transform.position = new Vector3(transform.position.x - 6f, transform.position.y, transform.position.z);
             }
             else if (currentScene == 3)
             {
-                OpenedDoor.transform.position = new Vector3(transform.position.x - 5f, transform.position.y, transform.position.z);
+                OpenedDoor.transform.position = new Vector3(transform.position.x - 6f, transform.position.y, transform.position.z);
             }
             else if (currentScene == 4)
             {
-                OpenedDoor.transform.position = new Vector3(transform.position.x - 5f, transform.position.y, transform.position.z); 
+                OpenedDoor.transform.position = new Vector3(transform.position.x - 6f, transform.position.y, transform.position.z); 
             }
         }
 	}
